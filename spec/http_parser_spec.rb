d95b819817e973e99ec809716055007188b1f025
Martin Terner <martin.terner@elev.ga.ntig.se>
2024-10-21 09:59:52 +0200

    consistent spec names and simplified spec code using before do

--- spec/http_parser_spec.rb
+++ spec/http_parser_spec.rb
@@ -3,88 +3,87 @@ require_relative '../lib/request'
 
 describe 'Request' do
 
-    describe 'Simple get-request' do
-    
-        it 'parses the http method' do
+    describe 'get-index.request' do
+        before do
             @request = Request.new(File.read('./spec/example_requests/get-index.request.txt'))
+        end
+
+        it 'parses the http method' do
             _(@request.method).must_equal "GET"
         end
 
         it 'parses the resource' do
-            @request = Request.new(File.read('./spec/example_requests/get-index.request.txt'))
             _(@request.resource).must_equal "/"
         end
         it 'parses the headers' do
-            @request = Request.new(File.read('./spec/example_requests/get-index.request.txt'))
             _(@request.headers["Host"]).must_equal "developer.mozilla.org"
             _(@request.headers["Accept-Language"]).must_equal "fr"
         end
     end
     describe 'get-examples.request' do
+
+        before do
+            @request = Request.new(File.read('./spec/example_requests/get-examples.request.txt'))
+        end
     
         it 'parses the http method' do
-            @request = Request.new(File.read('./spec/example_requests/get-examples.request.txt'))
             _(@request.method).must_equal "GET"
         end
 
         it 'parses the version' do
-            @request = Request.new(File.read('./spec/example_requests/post-login.request.txt'))
             _(@request.version).must_equal "HTTP/1.1"
         end
 
         it 'parses the resource' do
-            @request = Request.new(File.read('./spec/example_requests/get-examples.request.txt'))
             _(@request.resource).must_equal "/examples"
         end
         it 'parses the headers' do
-            @request = Request.new(File.read('./spec/example_requests/get-examples.request.txt'))
             _(@request.headers["Host"]).must_equal "example.com"
             _(@request.headers["User-Agent"]).must_equal "ExampleBrowser/1.0"
             _(@request.headers["Accept-Encoding"]).must_equal "gzip, deflate"
             _(@request.headers["Accept"]).must_equal "*/*"
         end
     end
-    describe 'get-fruits-with-filter-request' do
-        it 'parses the http method' do
+    describe 'get-fruits-with-filter.request' do
+        before do
             @request = Request.new(File.read('./spec/example_requests/get-fruits-with-filter.request.txt'))
+        end
+        
+        it 'parses the http method' do
             _(@request.method).must_equal "GET"
         end
 
         it 'parses the resource' do
-            @request = Request.new(File.read('./spec/example_requests/get-fruits-with-filter.request.txt'))
             _(@request.resource).must_equal "/fruits?type=bananas&minrating=4"
         end
         it 'parses the params' do
-            @request = Request.new(File.read('./spec/example_requests/get-fruits-with-filter.request.txt'))
             _(@request.params["type"]).must_equal "bananas"
             _(@request.params["minrating"]).must_equal "4"
         end
         it 'parses the headers' do
-            @request = Request.new(File.read('./spec/example_requests/get-fruits-with-filter.request.txt'))
             _(@request.headers["Host"]).must_equal "fruits.com"
             _(@request.headers["Accept-Encoding"]).must_equal "gzip, deflate"
             _(@request.headers["Accept"]).must_equal "*/*"
         end
     end
-    describe 'Simple post-request' do
+    describe 'post-login.request' do
+        before do
+            @request = Request.new(File.read('./spec/example_requests/post-login.request.txt'))
+        end
     
         it 'parses the http method' do
-            @request = Request.new(File.read('./spec/example_requests/post-login.request.txt'))
             _(@request.method).must_equal "POST"
         end
 
         it 'parses the resource' do
-            @request = Request.new(File.read('./spec/example_requests/post-login.request.txt'))
             _(@request.resource).must_equal "/login"
         end
 
         it 'parses the version' do
-            @request = Request.new(File.read('./spec/example_requests/post-login.request.txt'))
             _(@request.version).must_equal "HTTP/1.1"
         end
 
         it 'parses the headers' do
-            @request = Request.new(File.read('./spec/example_requests/post-login.request.txt'))
             _(@request.headers["Host"]).must_equal "foo.example"
             _(@request.headers["Content-Type"]).must_equal "application/x-www-form-urlencoded"
             _(@request.headers["Content-Length"]).must_equal "39"
