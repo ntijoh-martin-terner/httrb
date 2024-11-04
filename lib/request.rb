1555610da3f379be0a8f2b27b137a5ce5d440691
Martin Terner <martin.terner@elev.ga.ntig.se>
2024-11-04 09:16:39 +0100

    request rubocop refactor

--- lib/request.rb
+++ lib/request.rb
@@ -1,21 +1,49 @@
-class Request 
+# frozen_string_literal: true
+
+# Request
+class Request
   attr_reader :headers, :method, :params, :resource, :version
-  def initialize(request_string) 
-    head_and_body = request_string.split("\n\n", 2)
-    request_line_and_headers  = head_and_body[0].split("\n", 2)
 
-    @method, @resource, @version = request_line_and_headers [0].split(" ")
+  def initialize(request_string)
+    head, body = request_string.split("\n\n", 2)
+    request_line, headers = head.split("\n", 2)
 
-    @headers = request_line_and_headers [1].split("\n").map { |header| header_name, header_value = header.split(": ", 2); [header_name, header_value]  }.to_h()
-    
-    resource_params = @resource.split("?")[1]
+    @method, @resource, @version = request_line.split(' ')
 
-    @params = {}
-    
-    if !head_and_body[1].to_s.empty?
-      @params = head_and_body[1].split("&").map { |param| param_name, param_value = param.split("="); [param_name, param_value] }.to_h()
+    @headers = parse_headers(headers)
+
+    @params = parse_params(body, request_line)
+  end
+
+  def parse_headers(headers)
+    headers.split("\n").map do |header|
+      header_name, header_value = header.split(': ', 2)
+      [header_name, header_value]
+    end.to_h
+  end
+
+  def parse_params(body, request_line)
+    resource = request_line.split(' ')[1]
+    resource_params = resource.split('?')[1]
+
+    if !body.to_s.empty?
+      parse_head_params(body)
     elsif resource_params
-      @params = resource_params.split("&").map { |param| param_name, param_value = param.split("="); [param_name, param_value] }.to_h()
+      parse_resource_params(resource_params)
     end
+  end
+
+  def parse_resource_params(resource_params)
+    resource_params.split('&').map do |param|
+      param_name, param_value = param.split('=')
+      [param_name, param_value]
+    end.to_h
+  end
+
+  def parse_head_params(head)
+    head.split('&').map do |param|
+      param_name, param_value = param.split('=')
+      [param_name, param_value]
+    end.to_h
   end
 end
